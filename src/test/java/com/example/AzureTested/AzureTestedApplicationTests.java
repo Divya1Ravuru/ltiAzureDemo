058963313b35bedba09c9bd85aef2944d18bf1b3
prapulla <prapulla.r@toppr.com>
2021-11-12 20:00:49 +0530

    first commit of this application

---
+++ src/test/java/com/example/AzureTested/AzureTestedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AzureTested;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureTestedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
